b6dd523ac6beb92dc641babf5270b6ba47cf39b9
tonyjiangde <tao.jiang02@sap.com>
2020-04-16 22:31:05 +0800

    Update MyLocalMediaFileCacheService.java

--- core-customize/custom/training/src/org/training/platform/media/storage/impl/MyLocalMediaFileCacheService.java
+++ core-customize/custom/training/src/org/training/platform/media/storage/impl/MyLocalMediaFileCacheService.java
@@ -1,14 +1,10 @@
 package org.training.platform.media.storage.impl;
 
 
-import de.hybris.platform.core.Registry;
-import de.hybris.platform.media.storage.LocalMediaFileCacheService;
-import de.hybris.platform.media.storage.LocalStoringStrategy;
 import de.hybris.platform.media.storage.MediaMetaData;
 import de.hybris.platform.media.storage.MediaStorageConfigService;
 import de.hybris.platform.media.storage.MediaStorageConfigService.MediaFolderConfig;
 import de.hybris.platform.media.storage.MediaStorageRegistry;
-import de.hybris.platform.media.storage.MediaStorageStrategy;
 import de.hybris.platform.media.storage.impl.DefaultLocalMediaFileCacheService;
 import de.hybris.platform.media.storage.impl.DefaultMediaStorageConfigService.DefaultSettingKeys;
 import de.hybris.platform.media.storage.impl.LocalFileMediaStorageStrategy;
@@ -16,16 +12,11 @@ import de.hybris.platform.media.storage.impl.MediaCacheRecreator;
 import de.hybris.platform.media.storage.impl.MediaCacheRegion;
 import de.hybris.platform.media.storage.impl.StoredMediaData;
 import de.hybris.platform.regioncache.CacheController;
-import de.hybris.platform.regioncache.CacheLifecycleCallback;
 import de.hybris.platform.regioncache.CacheValueLoader;
 import de.hybris.platform.regioncache.key.CacheKey;
-import de.hybris.platform.regioncache.key.CacheUnitValueType;
-import de.hybris.platform.regioncache.region.CacheRegion;
 import de.hybris.platform.util.MediaUtil;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -33,22 +24,12 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Base64;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import javax.annotation.PostConstruct;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Required;
-
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
-import com.google.common.primitives.Ints;
 
 
 /**
@@ -64,7 +45,7 @@ import com.google.common.primitives.Ints;
  * folder.folderQualifier.local.cache = true
  * </pre>
  */
-public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
+public class MyLocalMediaFileCacheService extends DefaultLocalMediaFileCacheService
 {
 	private static final Logger LOG = Logger.getLogger(DefaultLocalMediaFileCacheService.class);
 	private static final String DEFAULT_CACHE_FOLDER = "cache";
@@ -81,38 +62,13 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 
 	private String tenantId;
 
-	/**
-	 * Recreates cache and adds lifecycle callback after bean construction
-	 */
-	@PostConstruct
-	public void init()
-	{
-		tenantId = Registry.getCurrentTenantNoFallback().getTenantID();
-		cacheController.addLifecycleCallback(new MediaCacheLifecycleCallback());
-		cacheRecreator.recreateCache(storageConfigService.getDefaultCacheFolderName(), getRemoteStorageFolderConfigs());
-	}
 
-	private Iterable<MediaFolderConfig> getRemoteStorageFolderConfigs()
-	{
-		final Set<MediaFolderConfig> result = new HashSet<>();
 
-		final Map<String, MediaStorageStrategy> allStrategies = storageRegistry.getStorageStrategies();
-		for (final Map.Entry<String, MediaStorageStrategy> entry : allStrategies.entrySet())
-		{
-			final String strategyId = entry.getKey();
-			final MediaStorageStrategy strategy = entry.getValue();
-			if (!(strategy instanceof LocalStoringStrategy))
-			{
-				Iterables.addAll(result, storageConfigService.getFolderConfigsForStrategy(strategyId));
-			}
-		}
-
-		return result;
-	}
-
+	//-----------------------------------------
 	@Override
 	public File storeOrGetAsFile(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
 	{
+
 		final File file = getMediaCacheFile(config, location, streamGetter);
 
 		if (file == null)
@@ -123,6 +79,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		return file;
 	}
 
+	//-----------------------------------------
 	private File getMediaCacheFile(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
 	{
 		final File file;
@@ -146,6 +103,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		return file;
 	}
 
+
 	private static File getStreamAsTempFile(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
 	{
 		File file = null;
@@ -168,6 +126,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		return file;
 	}
 
+	//-----------------------------------------
 	@Override
 	public InputStream storeOrGetAsStream(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
 	{
@@ -181,6 +140,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		return stream;
 	}
 
+	//-----------------------------------------
 	private InputStream getMediaCacheStream(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
 	{
 		final InputStream stream;
@@ -214,6 +174,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		return cacheUnitWeight > mediaCacheRegion.getCacheMaxEntries();
 	}
 
+	//-----------------------------------------
 	private abstract class CacheResourceLoader<T>
 	{
 		public T loadResource(final MediaFolderConfig config, final String location, final StreamGetter streamGetter)
@@ -238,6 +199,18 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 
 	}
 
+	private static String getCacheFolderPath(final MediaFolderConfig config)
+	{
+		final String rootCacheFolder = getCacheFolder(config);
+		return rootCacheFolder + MediaUtil.FILE_SEP + config.getFolderQualifier();
+	}
+
+	private static String getCacheFolder(final MediaFolderConfig config)
+	{
+		return config.getParameter(DefaultSettingKeys.LOCAL_CACHE_ROOT_FOLDER_KEY.getKey(), String.class, DEFAULT_CACHE_FOLDER);
+	}
+
+	//-----------------------------------------
 	/**
 	 * Loads media binary data from remote storage and writes to disk cache.
 	 */
@@ -281,7 +254,7 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 			final String encodedLocation = Base64.getUrlEncoder().encodeToString(location.getBytes());
 			final StringBuilder builder = new StringBuilder(encodedLocation);
 			builder.append(CACHE_FILE_NAME_DELIM).append(UUID.randomUUID());
-			return builder.toString();
+			return String.valueOf(builder.toString().hashCode());
 		}
 
 		public boolean isLoaded(final MediaCacheUnit cacheUnit)
@@ -291,403 +264,4 @@ public class MyLocalMediaFileCacheService implements LocalMediaFileCacheService
 		}
 	}
 
-	@Override
-	public void removeFromCache(@SuppressWarnings("unused")
-	final MediaFolderConfig config, final String location)
-	{
-		cacheController.invalidate(new MediaCacheKey(Registry.getCurrentTenant().getTenantID(), getCacheFolder(config), location));
-	}
-
-	private static String getCacheFolderPath(final MediaFolderConfig config)
-	{
-		final String rootCacheFolder = getCacheFolder(config);
-		return rootCacheFolder + MediaUtil.FILE_SEP + config.getFolderQualifier();
-	}
-
-	private static String getCacheFolder(final MediaFolderConfig config)
-	{
-		return config.getParameter(DefaultSettingKeys.LOCAL_CACHE_ROOT_FOLDER_KEY.getKey(), String.class, DEFAULT_CACHE_FOLDER);
-	}
-
-	public static class MediaCacheFileInputStream extends FileInputStream
-	{
-		private final MediaCacheUnit cacheUnit;
-
-		public MediaCacheFileInputStream(final File file, final MediaCacheUnit cacheUnit) throws FileNotFoundException
-		{
-			super(file);
-			this.cacheUnit = cacheUnit;
-		}
-
-		@Override
-		public void close() throws IOException
-		{
-			super.close();
-			cacheUnit.releaseUsageAndRemoveIfMarked();
-		}
-	}
-
-	public static class MediaCacheKey implements CacheKey
-	{
-		private static final String MEDIA_CACHE_UNIT_CODE = "__MEDIA__";
-		private final String tenantId;
-		private final String location;
-		private final String cacheFolder;
-
-		public MediaCacheKey(final String tenantId, final String cacheFolder, final String location)
-		{
-			this.tenantId = tenantId;
-			this.cacheFolder = cacheFolder;
-			this.location = location;
-		}
-
-		@Override
-		public CacheUnitValueType getCacheValueType()
-		{
-			return CacheUnitValueType.NON_SERIALIZABLE;
-		}
-
-		@Override
-		public Object getTypeCode()
-		{
-			return MEDIA_CACHE_UNIT_CODE + cacheFolder;
-		}
-
-		@Override
-		public String getTenantId()
-		{
-			return tenantId;
-		}
-
-		@Override
-		public int hashCode()
-		{
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((cacheFolder == null) ? 0 : cacheFolder.hashCode());
-			result = prime * result + ((location == null) ? 0 : location.hashCode());
-			result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(final Object obj)
-		{
-			if (this == obj)
-			{
-				return true;
-			}
-			if (obj == null)
-			{
-				return false;
-			}
-			if (getClass() != obj.getClass())
-			{
-				return false;
-			}
-			final MediaCacheKey other = (MediaCacheKey) obj;
-			if (cacheFolder == null)
-			{
-				if (other.cacheFolder != null)
-				{
-					return false;
-				}
-			}
-			else if (!cacheFolder.equals(other.cacheFolder))
-			{
-				return false;
-			}
-			if (location == null)
-			{
-				if (other.location != null)
-				{
-					return false;
-				}
-			}
-			else if (!location.equals(other.location))
-			{
-				return false;
-			}
-			if (tenantId == null)
-			{
-				if (other.tenantId != null)
-				{
-					return false;
-				}
-			}
-			else if (!tenantId.equals(other.tenantId))
-			{
-				return false;
-			}
-			return true;
-		}
-
-		@Override
-		public String toString()
-		{
-			return "MediaCacheKey [tenantId=" + tenantId + ", location=" + location + ", cacheFolder=" + cacheFolder + "]";
-		}
-	}
-
-	public static class MediaCacheUnit
-	{
-		private static final Logger LOG = Logger.getLogger(MediaCacheUnit.class);
-		public static final int MIN_UNIT_WEIGHT_IN_KB = 1;
-
-		private final File cachedFile;
-
-		private volatile boolean markedAsEvicted = false;
-		private volatile boolean takenAsFile = false;
-		private final AtomicInteger usageCounter = new AtomicInteger();
-
-		public MediaCacheUnit(final File cachedFile)
-		{
-			Preconditions.checkNotNull(cachedFile, "Cached file must not be null");
-			Preconditions.checkArgument(cachedFile.exists(),
-					"MediaCacheUnit may only cache existing files [file: " + cachedFile.getAbsolutePath() + " does not exist]");
-			this.cachedFile = cachedFile;
-		}
-
-		public boolean isCachedFileExists()
-		{
-			return cachedFile != null && cachedFile.exists();
-		}
-
-		/**
-		 * This method returns direct cached File instance. It marks cache unit with that information thus any possible
-		 * eviction to that unit will write special zero-bytes marker file in cache folder and will NOT remove file
-		 * itself. All marked cache files will be removed on next system restart or may be removed by system administrator
-		 * manually. Remember that in case of getting cache files as regular File instances may exceed declared cache size
-		 * due to evictions.
-		 *
-		 * @return cached file instance
-		 */
-		public synchronized File getFile()
-		{
-			if (markedAsEvicted || !cachedFile.exists())
-			{
-				return null;
-			}
-
-			takenAsFile = true;
-			return cachedFile;
-		}
-
-		public synchronized InputStream getStream()
-		{
-			if (isMarkedAsEvictedAndNotUsed())
-			{
-				return null;
-			}
-
-			try
-			{
-				usageCounter.incrementAndGet();
-				return new MediaCacheFileInputStream(cachedFile, this);
-			}
-			catch (final FileNotFoundException e)
-			{
-				throw new IllegalStateException(e.getMessage(), e);
-			}
-		}
-
-		private boolean isMarkedAsEvictedAndNotUsed()
-		{
-			return markedAsEvicted && usageCounter.get() <= 0;
-		}
-
-		public synchronized void markResourceAsEvicted()
-		{
-			markedAsEvicted = true;
-		}
-
-		protected void releaseUsageAndRemoveIfMarked()
-		{
-			final int currentCounter = usageCounter.decrementAndGet();
-			if (LOG.isDebugEnabled())
-			{
-				LOG.debug(this + " usage was released [usage counter: " + currentCounter + "]");
-			}
-
-			tryRemoveResourceOrWriteEvictedMarker();
-		}
-
-		public synchronized void tryRemoveResourceOrWriteEvictedMarker()
-		{
-			if (canRemove())
-			{
-				removeResource();
-			}
-			else if (canWriteEvictedMarkerFile())
-			{
-				writeEvictedMarkerFile();
-			}
-		}
-
-		private synchronized boolean canRemove()
-		{
-			if (LOG.isDebugEnabled())
-			{
-				LOG.debug("Can remove cached file? ==> " + (markedAsEvicted && usageCounter.get() <= 0 && !takenAsFile)
-						+ " [markedAsEvicted: " + markedAsEvicted + ", usageConuter: " + usageCounter.get() + ", takenAsFile: "
-						+ takenAsFile + ", cachedFile: " + cachedFile + "]");
-			}
-			return markedAsEvicted && usageCounter.get() <= 0 && !takenAsFile;
-		}
-
-		private synchronized boolean canWriteEvictedMarkerFile()
-		{
-			return markedAsEvicted && takenAsFile && isCachedFileExists();
-		}
-
-		/**
-		 * Removes resource without any checks. Unsafe and not thread safe!
-		 */
-		protected void removeResource()
-		{
-			if (isCachedFileExists())
-			{
-				final boolean isDeleted = cachedFile.delete();
-				logFileDeletion(isDeleted);
-			}
-		}
-
-		private void logFileDeletion(final boolean isDeleted)
-		{
-			if (isDeleted)
-			{
-				if (LOG.isDebugEnabled())
-				{
-					LOG.debug("Removed cached file: " + cachedFile);
-				}
-			}
-			else
-			{
-				LOG.error("Cannot remove cached file");
-			}
-		}
-
-		private void writeEvictedMarkerFile()
-		{
-			try
-			{
-				final File markerFile = new File(cachedFile.getAbsolutePath() + ".EVICTED");
-				markerFile.createNewFile();
-			}
-			catch (final IOException e)
-			{
-				throw new IllegalStateException(e.getMessage(), e);
-			}
-		}
-
-		public int getWeight()
-		{
-			return convertNumBytesToCacheUnitWeight(cachedFile.length());
-		}
-
-		public static int convertNumBytesToCacheUnitWeight(final long numBytes)
-		{
-			final long val = numBytes / 1024;
-			return val == 0 ? MIN_UNIT_WEIGHT_IN_KB : Ints.checkedCast(val);
-		}
-
-		@Override
-		public String toString()
-		{
-			return "MediaCacheUnit [cachedFile=" + cachedFile + ", markedAsEvicted=" + markedAsEvicted + ", usageCounter="
-					+ usageCounter + "]";
-		}
-
-	}
-
-	private static class MediaCacheLifecycleCallback implements CacheLifecycleCallback
-	{
-
-		@Override
-		public void onAfterRemove(final CacheKey key, final Object value, final CacheRegion region)
-		{
-			markAsEvictedAndTryRemove(value);
-		}
-
-		@Override
-		public void onAfterEviction(final CacheKey key, final Object value, final CacheRegion region)
-		{
-			markAsEvictedAndTryRemove(value);
-		}
-
-		private static void markAsEvictedAndTryRemove(final Object cacheUnit)
-		{
-			if (cacheUnit instanceof MediaCacheUnit)
-			{
-				if (LOG.isDebugEnabled())
-				{
-					LOG.debug("Trying to remove cached file on eviction event [cacheUnit: " + cacheUnit + "]");
-				}
-
-				((MediaCacheUnit) cacheUnit).markResourceAsEvicted();
-				((MediaCacheUnit) cacheUnit).tryRemoveResourceOrWriteEvictedMarker();
-			}
-		}
-
-		@Override
-		public void onAfterAdd(final CacheKey key, final Object value, final CacheRegion region)
-		{
-			// no need to implement
-		}
-
-		@Override
-		public void onMissLoad(final CacheKey key, final Object value, final CacheRegion lruCacheRegion)
-		{
-			if (value instanceof MediaCacheUnit)
-			{
-				if (LOG.isDebugEnabled())
-				{
-					LOG.debug("Trying to remove file for unit " + value);
-				}
-				((MediaCacheUnit) value).removeResource();
-			}
-		}
-	}
-
-	@Required
-	public void setMainDataDir(final File mainDataDir)
-	{
-		this.mainDataDir = mainDataDir;
-	}
-
-	@Required
-	public void setCacheController(final CacheController cacheController)
-	{
-		this.cacheController = cacheController;
-	}
-
-	@Required
-	public void setStorageRegistry(final MediaStorageRegistry storageRegistry)
-	{
-		this.storageRegistry = storageRegistry;
-	}
-
-	@Required
-	public void setStorageStrategy(final LocalFileMediaStorageStrategy storageStrategy)
-	{
-		this.storageStrategy = storageStrategy;
-	}
-
-	@Required
-	public void setMediaCacheRegion(final MediaCacheRegion mediaCacheRegion)
-	{
-		this.mediaCacheRegion = mediaCacheRegion;
-	}
-
-	@Required
-	public void setStorageConfigService(final MediaStorageConfigService storageConfigService)
-	{
-		this.storageConfigService = storageConfigService;
-	}
-
-	@Required
-	public void setCacheRecreator(final MediaCacheRecreator cacheRecreator)
-	{
-		this.cacheRecreator = cacheRecreator;
-	}
 }
